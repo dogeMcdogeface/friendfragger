90e671b731a433a0ca2ecd381b0b1507727b3f45
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2025-05-15 02:31:55 +0200

    browser compatibility

--- builds/html/index.service.worker.js
+++ builds/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747268682|4633036258';
+const CACHE_VERSION = '1747269096|5046680194';
 /** @type {string} */
 const CACHE_PREFIX = 'FriendFragger-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
